361fe69dd8ad69258282066eba3126d404404cef
Tusar <tusarahmed141@gmail.com>
2024-12-13 22:57:40 +0600

    First express function run

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello Wold!");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get('/profile', (req, res) => {
+    res.send("<h1>Tusar</h1>")
+})
+
+app.listen(port, () => {
+  console.log(`Listening Successfull: ${port}`);
+});
